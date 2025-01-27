f9c9524bf9da617bec3423b10b9c192f9048d79d
Ashim Gautam <computer77356@gmail.com>
2025-01-27 13:40:24 +0545

    created account router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import { userRouter } from './user';
-
+import { accountRouter } from './accounts';
 
 export const router = express.Router();
 
 
 router.use('/user',userRouter)
+router.use('/account', accountRouter)
 
 
