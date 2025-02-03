6336c24464c80d54e056e789d9f2090269280384
Ashim Gautam <computer77356@gmail.com>
2025-02-03 15:35:20 +0545

    added address in vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    port: 10000,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 10000,
+    allowedHosts: ["paytmclone-frontend.onrender.com"],
+  },
 })
