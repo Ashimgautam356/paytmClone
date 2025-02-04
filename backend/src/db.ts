dbae1559772377a1b6acc3dee5a9d9ec49be852c
Ashim Gautam <computer77356@gmail.com>
2025-02-04 14:02:30 +0545

    crated a transaction Pin in db

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -33,7 +33,7 @@ const UserSchema =new  mongoose.Schema({
         maxLength:40,
     }
 
-})
+},{timestamps:true})
 
 const AccountTable = new mongoose.Schema({
     userId:{type:mongoose.Types.ObjectId, require:true, unique:true, ref:"User"},
@@ -44,10 +44,18 @@ const StatementTable = new mongoose.Schema({
     userId:{type:mongoose.Types.ObjectId, require:true, unique:true, ref:"User"},
     method:{type:String, require:true},
     remarks:{type:String},
-    balance:{type:Number,min:0}, 
-})
+    balance:{type:Number,min:0}
+},{timestamps:true})
+
+const TransactionPin = new mongoose.Schema({
+    userId:{type:mongoose.Types.ObjectId, require:true, unique:true, ref:"User"},
+    transactionPin:{type:String,require:true},
+    failedAttempts:{type:Number,default:0}
+},{timestamps:true})
+
 
 export const UserModel = mongoose.model("User",UserSchema)
 export const AccountModel = mongoose.model('Account',AccountTable)
 export const StatementModel = mongoose.model('Statement',StatementTable)
+export const PinModel = mongoose.model('TransactionPin_pin',TransactionPin)
 
